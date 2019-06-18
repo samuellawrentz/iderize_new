3e69fff287da5483d7f1c4e0421ea1e6f92d207f
Samuel Lawerence <>
2019-06-18 08:01:50 +0530

    Changed the services section and made tweak in font-size

--- script.js
+++ script.js
@@ -2,31 +2,31 @@ var servicesContent = [
     {
         name: 'Software Development',
         desc: 'We combine a creative design approach with the latest software and hardware engineering principles to ensure a spectacular result.',
-        listItems : ['Python Backend', 'Frontend Engineering', 'DB Design', 'Product Architecture', 'Site Development'],
+        listItems : ['Design Thinking', 'UX/UI Design', 'Wireframing', 'User Centric Design', 'Market Research'],
         additionalText: 'Using proven methods combined with today’s most advanced tools and data, we can provide you with the edge you need to dominate your niche.'
     },
     {
         name: 'Product Development',
         desc: 'For a business to succeed in today’s fast-paced market, agility is number one. We give you the tools you need to move forward with confidence, fully customized to your needs.',
-        listItems : ['Design Thinking', 'UX/UI Design', 'Wireframing', 'User Centric Design', 'Market Research'],
+        listItems : ['Frontend Development', 'Backend Development', 'Mobile App Development', 'ERP Systems', 'APIs & Web Services'],
         additionalText: 'Support your teams with systems that make sense. Delight your customers by giving them what they want. It’s all possible.'
     },
     {
         name: 'Software Testing',
         desc: 'Testing and quality assurance are critical aspects of software and hardware development. Our testing suite leaves nothing to chance, ensuring your products perform exactly as they should.',
-        listItems : ['A/B Testing', 'Selenium', 'Test Cases', 'Regression Testing', 'Manual Stage Testing'],
+        listItems : ['Functional QA &amp; Testing', 'Mobile Testing', 'In-Sprint Automation', 'Performance Testing', 'Security Testing'],
         additionalText: 'Most software failures that occur could have been easily avoided with performance testing. Testing reduces downtime, identifies vulnerabilities, and assures quality from day one.'
     },
     {
         name: 'Innovation',
         desc: 'Today’s most advanced tools and technologies can be applied to your business, improving processes from manufacturing to logistics to customer service, and more besides.',
-        listItems : ['Home Automation', 'Drones', 'i Toys', 'Epayment', 'Bio Tech'],
+        listItems : ['Blockchain', 'Machine Learning', 'Artificial Intelligence', 'Internet of Things', 'Chatbots and Virtual Assistants'],
         additionalText: 'Discover what these innovations can do to elevate your business processes and support scale.'
     },
     {
         name: 'Salesforce',
         desc: 'Salesforce is one of the most powerful CRMs on the market today, but it reaches its full potential only when it is customized to integrate with your business systems.',
-        listItems : ['Analytics', 'Data Mining', 'Hadoop', 'Tie2', 'SFC PRogram'],
+        listItems : ['Lightning Implementation & Migration', 'Admin, Development & Support Services', 'Sales, Service & Marketing Cloud Implementation', 'Pardot Marketing Automation', 'Nintex Drawloop Integration & DocuSign Integration'],
         additionalText: 'Working closely with you, we will design, configure, and help you implement a dynamic Salesforce workflow.'
     }
 ]
